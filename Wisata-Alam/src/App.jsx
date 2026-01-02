68c947805c8e07f6c110923a2bb25df49bfa93b4
ahmadalwan <madajadeh2023@gmail.com>
2026-01-02 16:07:12 +0700

    Memperbaiki isi folder masing-masing

--- Wisata-Alam/src/App.jsx
+++ Wisata-Alam/src/App.jsx
@@ -13,9 +13,11 @@ function App() {
 
       <Header Hero={"Selamat Datang di Website Kami"}/>
 
+      <Header imgSrc={"https://picsum.photos/200/300"} imgAlt={"Gambar ini tidak mendukung di perangkat anda"} />
+
       <About about={ "NusaGo adalah sebuah website wisata alam yang menyediakan beberapa layanan terbaik sepanjang masa" }/>
 
-      <Customer p={"Daftar Pelanggan Setia Kami:"} c1={"Bayu - Asal Cibereum"} c2={"Yanto - Jakarta"} c3={"Ucup - Bogor"}/>
+      <Customer p={"Harga Tiket Masuk Wisata Depok:"} c1={"Wisata Depok/1 - Rp.15.000"} c2={"Wisata Depok/2 - Rp.10.000"} c3={"Curug Pekayon - Rp.30.000"}/>
 
       <Footer footer={"Copyright &copy;2026 Developed by NusaGo ❤️"}/>
     </>
